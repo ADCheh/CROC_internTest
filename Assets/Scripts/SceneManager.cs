6935b3807712a41ddfe777088927636f8491ff7c
ADCheh <tashuvalov@gmail.com>
2021-02-12 15:59:53 +0300

    mistakes+scenes

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void NextScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void RestartScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
